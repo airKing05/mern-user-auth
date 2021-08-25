6e4f460d5fc1088e6812bf6190af29698b7c41b4
anilraj <anil050797@gmial.com>
2021-08-26 03:36:22 +0530

    initial commit

--- mern-july1/client-code/src/App.js
+++ mern-july1/client-code/src/App.js
@@ -4,10 +4,12 @@ import Home from './components/Home';
 import About from './components/About';
 import SignUp from './components/SignUp';
 import Signin from './components/Signin';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
      <Home/>
      <About/>
      <SignUp/>
