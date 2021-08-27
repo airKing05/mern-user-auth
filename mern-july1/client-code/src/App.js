5dc537624e893b97ce88fda34bca9da62fe5104b
anilraj <anil050797@gmial.com>
2021-08-28 04:43:11 +0530

    home first commit

--- mern-july1/client-code/src/App.js
+++ mern-july1/client-code/src/App.js
@@ -3,7 +3,7 @@ import './style.css';
 import Home from './components/Home';
 import About from './components/About';
 import SignUp from './components/SignUp';
-import SignIn from './components/SignIn';
+import Signin from './components/Signin';
 import Navbar from './components/Navbar';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Contact from './components/Contact';
@@ -17,7 +17,7 @@ function App() {
         <Route path = '/about' component ={About}/>
         <Route path = '/contact' component ={Contact}/>
         <Route path = '/sign-up' component ={SignUp}/>
-        <Route path = '/sign-in' component ={SignIn} />
+        <Route path = '/sign-in' component ={Signin} />
       </Switch>
     </BrowserRouter>
   );
--- mern-july1/client-code/src/components/Home.js
+++ mern-july1/client-code/src/components/Home.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div>
-            this is home page after the git added
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-6" style={{background: 'gray', height: '100vh'}}></div>
+                <div className="col-6" style={{background: 'darkgray'}}></div>
+                <div className="middle" style={{position:'absolute', top:'45%', margin: '10px auto'}}>
+                <h4 className="middle" >Welcome visitors</h4>
+                <h3 className="middle">this is website is for user authentication</h3>
+                </div>
+            </div>
         </div>
     )
 }
