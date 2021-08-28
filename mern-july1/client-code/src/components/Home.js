4c5e053ea2cdb73042fe972082162da8904ea0a2
anilraj <anil050797@gmial.com>
2021-08-29 02:27:46 +0530

    text fixed

--- mern-july1/client-code/src/components/Home.js
+++ mern-july1/client-code/src/components/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
                 <div className="col-6" style={{background: 'darkgray'}}></div>
                 <div className="container " style={{position:'absolute', top:'50%', margin: '10px auto'}}>
                 <h4 className=" row d-flex justify-content-center " style={{position:'relative', top:'0px', }}>Welcome visitors</h4>
-                <h3 className=" row d-flex justify-content-center " style={{position:'relative', top:'0px', }}>this is website is for user authentication</h3>
+                <h3 className=" row d-flex justify-content-center " style={{position:'relative', top:'0px', }}>this website is for user authentication</h3>
                 </div>
             </div>
         </div>
