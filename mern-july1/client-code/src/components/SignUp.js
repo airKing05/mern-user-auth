6b8feaed8eaea8ca17c4e63c0ceaced1e907a179
anilraj <anil050797@gmial.com>
2021-08-30 16:50:59 +0530

    state in signup

--- mern-july1/client-code/src/components/SignUp.js
+++ mern-july1/client-code/src/components/SignUp.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import signUpImg from '../images/signup.jpg';
 
 export default function SignUp() {
+    const [user, setUser] = useState({
+        name : "",
+        email: "",
+        phone: "",
+        work: "",
+        password: "",
+        cpassword: ""
+
+    })
+
+    let name, value;
+    const handelInput = (e) => {
+         console.log(e)
+          name = e.target.name; 
+          value = e.target.value;
+
+          setUser({ ... user , [name]: value})
+    }
     return (
         <div className="container cont" style={{ maxWidth: '700px', marginTop: '100px', marginBottom: '100px' }}>
             <div className="row px-5 pt-5  pb-3">
@@ -12,23 +30,41 @@ export default function SignUp() {
             <div className="row px-5 pb-4">
                 <div className="col-md-6">
                     <form>
-                        <div class="mb-3 "> 
-                            <input type="text" class="form-control border-top-0 border-left-0 border-right-0 rounded-0  border-dark" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name" />
+                        <div class="mb-3 form-group"> 
+                            <input type="text" class="form-control border-top-0 border-left-0 border-right-0 rounded-0  border-dark" autoComplete="off" id="name" 
+                            name="name"
+                            value={user.name}
+                            onChange={handelInput} placeholder="Enter Name" />
                         </div>
-                        <div class="mb-3"> 
-                            <input type="email" class="form-control  border-top-0 border-left-0 border-right-0 rounded-0  border-dark" id="exampleInputEmail1" aria-describedby="emailHelp"  placeholder="Enter Email"/>
+                        <div class="mb-3 form-group"> 
+                            <input type="email" class="form-control  border-top-0 border-left-0 border-right-0 rounded-0  border-dark" autoComplete="off" id="email"
+                            name="email" 
+                            value={user.email}
+                            onChange={handelInput}  placeholder="Enter Email"/>
                         </div>
-                        <div class="mb-3"> 
-                            <input type="number" class="form-control border-top-0 border-left-0 border-right-0 rounded-0  border-dark" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Contact Number"/>
+                        <div class="mb-3 form-group"> 
+                            <input type="number" class="form-control border-top-0 border-left-0 border-right-0 rounded-0  border-dark" autoComplete="off" id="phone"
+                            name="phone" 
+                            value={user.phone}
+                            onChange={handelInput} placeholder="Enter Contact Number"/>
                         </div>
-                        <div class="mb-3"> 
-                            <input type="text" class="form-control  border-top-0 border-left-0 border-right-0 rounded-0  border-dark" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Profession"/>
+                        <div class="mb-3 form-group"> 
+                            <input type="text" class="form-control  border-top-0 border-left-0 border-right-0 rounded-0  border-dark" autoComplete="off" id="work" 
+                            name="work"
+                            value={user.work}
+                            onChange={handelInput} placeholder="Your Profession"/>
                         </div>
-                        <div class="mb-3">
-                            <input type="password" class="form-control border-top-0 border-left-0 border-right-0 rounded-0  border-dark" id="exampleInputPassword1" placeholder="Password" />
+                        <div class="mb-3 form-group">
+                            <input type="password" class="form-control border-top-0 border-left-0 border-right-0 rounded-0  border-dark" autoComplete="off" id="password"
+                            name="password"
+                            value={user.password}
+                            onChange={handelInput} placeholder="Password" />
                         </div>
-                        <div class="mb-3">
-                            <input type="password" class="form-control  border-top-0 border-left-0 border-right-0 rounded-0  border-dark" id="exampleInputPassword1" placeholder="Confirm Password" />
+                        <div class="mb-3 form-group">
+                            <input type="password" class="form-control  border-top-0 border-left-0 border-right-0 rounded-0  border-dark" autoComplete="off" id="cpassword"
+                            name="cpassword" 
+                            value={user.cpassword}
+                            onChange={handelInput} placeholder="Confirm Password" />
                         </div>
                         
                         <button type="submit" class="btn btn-primary d-grid gap-2 col-12">Submit</button>
